e05b8bbdc111a7637f89872c247baade6f0b9d87
Thomas Levine <occurrence@thomaslevine.com>
2013-04-10 21:56:27 -0400

    point-free

--- 2008.1A/run.hs
+++ 2008.1A/run.hs
@@ -4,30 +4,28 @@ type Vector = [Integer]
 type Case = (Integer, Vector, Vector)
 
 -- Import
+dataImport :: String -> [Case]
+dataImport = ((caseify 1) . tail) . lines
+
 caseify :: Integer -> [String] -> [Case]
 caseify n [] = []
 caseify n (_:v1:v2:rest) = (n, vectorize v1, vectorize v2):(caseify (n + 1) rest)
 
 vectorize :: String -> Vector
-vectorize v = map (\w -> (read w :: Integer)) $ words v
+vectorize = (map (\w -> (read w :: Integer))) . words
 
 -- Scalar product
 minimumScalarProduct :: Vector -> Vector -> Integer
 minimumScalarProduct v1 v2 = sum $ (zipWith (*)) (sort v1) (reverse $ sort v2)
 
--- Print
+-- Output
 prettyCase :: Case -> String
 prettyCase (n,v1,v2) = "Case #" ++ (show n) ++ ": " ++ (show msp)
   where
     msp = minimumScalarProduct v1 v2
 
-dataImport :: String -> [Case]
--- dataImport = lines . tail . caseify
-dataImport s = caseify 1 $ tail $ lines s
-
-go input = unlines $ map prettyCase $ dataImport input
-
 -- Go
+go = unlines . (map prettyCase) . dataImport
 main = do
   small <- readFile "A-small-practice.in"
   writeFile "A-small-practice.out" (go small)
