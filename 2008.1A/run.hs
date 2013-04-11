81ea716ce8677d5a898e1071edace35022cca97b
Thomas Levine <occurrence@thomaslevine.com>
2013-04-10 21:08:01 -0400

    aoeu

--- 2008.1A/run.hs
+++ 2008.1A/run.hs
@@ -1,13 +1,25 @@
-v1 :: [Integer]
+type Vector = [Integer]
+type Case = (Vector, Vector)
+
+v1 :: Vector
 v1 = [1..3]
 
-v2 :: [Integer]
+v2 :: Vector
 v2 = [0..2]
 
 
-scalarProduct :: [Integer] -> [Integer] -> [Integer]
+scalarProduct :: Vector -> Vector -> Vector
 scalarProduct = zipWith (*)
 
 
+caseify :: [String] -> [Case]
+caseify (n:v1:v2:rest) = (vectorize v1, vectorize v2):(caseify rest)
+caseify [] = []
+
+vectorize :: String -> Vector
+vectorize _ = [1,2,3] -- words . (map (\w -> (read w :: Integer)))
+
 main = do
-  putStrLn $ show $ scalarProduct v1 v2
+  input <- readFile "A-small-practice.in"
+  -- putStrLn $ show $ caseify $ tail $ lines input
+  putStrLn $ show $ caseify $ tail $ take (9 + 1) $ lines input
