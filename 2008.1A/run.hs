a9eb6a3beeb4faac8a805e725faf2baef8287413
Thomas Levine <occurrence@thomaslevine.com>
2013-04-10 21:32:35 -0400

    aoeu

--- 2008.1A/run.hs
+++ 2008.1A/run.hs
@@ -1,23 +1,30 @@
 import Data.List (sort)
 
 type Vector = [Integer]
-type Case = (Vector, Vector)
+type Case = (Integer, Vector, Vector)
 
 -- Import
 caseify :: [String] -> [Case]
 caseify [] = []
-caseify (n:v1:v2:rest) = (vectorize v1, vectorize v2):(caseify rest)
+caseify (n:v1:v2:rest) = ((read n :: Integer), vectorize v1, vectorize v2):(caseify rest)
 
 vectorize :: String -> Vector
 vectorize v = map (\w -> (read w :: Integer)) $ words v
 
 -- Scalar product
-minimumScalarProduct :: (Vector, Vector) -> Integer
-minimumScalarProduct (v1, v2) = sum $ (zipWith (*)) (sort v1) (reverse $ sort v2)
+minimumScalarProduct :: Vector -> Vector -> Integer
+minimumScalarProduct v1 v2 = sum $ (zipWith (*)) (sort v1) (reverse $ sort v2)
+
+-- Print
+prettyCase :: Case -> String
+prettyCase (n,v1,v2) = "Case #" ++ (show n) ++ ": " ++ (show msp)
+  where
+    msp = minimumScalarProduct v1 v2
+
+dataImport :: String -> [Case]
+dataImport s = caseify $ lines $ tail s
 
 -- Go
 main = do
   input <- readFile "A-small-practice.in"
-  -- putStrLn $ show $ caseify $ tail $ lines input
-  -- putStrLn $ show $ minimumScalarProduct caseify $ tail $ take (9 + 1) $ lines input
-  putStrLn input
+  putStrLn $ unlines $ map prettyCase $ dataImport input
